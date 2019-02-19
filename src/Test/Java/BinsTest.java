dc8aadcfd9078b00b1add9ff6114a7e28dfb70dd
Davis Perchik <davisperchik@gmail.com>
2019-02-19 17:57:56 -0500

    Simulation runs, all tests complete

--- src/Test/Java/BinsTest.java
+++ src/Test/Java/BinsTest.java
@@ -1,9 +1,19 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BinsTest {
 
     @Test
-    public void testBins1(){
-        
+    public void testStoreBins(){
+        //Given
+        Bins bins = new Bins(2,12);
+
+        //When
+        bins.storeBins(5);
+        Integer actual = bins.getBins(5);
+
+        //Then
+        Integer expected = 1;
+        Assert.assertEquals(expected, actual);
     }
 }
--- src/Test/Java/DiceTest.java
+++ src/Test/Java/DiceTest.java
@@ -1,9 +1,21 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DiceTest {
 
     @Test
-    public void testSumAndToss(){
+    public void testSumAndToss() {
+        //Given
+        Dice dice = new Dice(2);
 
+        //When
+        Integer actual = dice.tossAndSum();
+
+        //Then
+        Boolean expected = false;
+        if (actual >= 2 && actual <= 12) {
+            expected = true;
+        }
+        Assert.assertTrue(expected);
     }
 }
